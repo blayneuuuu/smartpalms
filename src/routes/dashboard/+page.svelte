19125b9b5afa8dcd56fc92a2467164bfd6232ae1
jbprog14 <jbprog14@gmail.com>
2025-01-11 01:08:01 +0800

    Dashboard UI

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -11,11 +11,27 @@
     const id = $derived(data?.user?.id);
     const email = $derived(data?.user?.emailAddresses[0].emailAddress);
     const fullName = $derived(data?.user?.fullName || "User");
+    const imageUrl = $derived(data?.user?.imageUrl);
 </script>
 
 <div>
-    <span>{fullName}</span>
-    <span>{email}</span>
-    <span>{id}</span>
-    <UserButton afterSignOutUrl="/"/>
+    <div class="flex flex-row justify-between py-2 px-20 items-center">
+        <h1 class="text-2xl font-extrabold">Dashboard</h1>
+        <div class="flex flex-row items-center space-x-4">
+            <h1 class="text-lg font-semibold">{fullName}</h1>
+            <UserButton afterSignOutUrl="/"/>
+        </div>
+    </div>
+
+    <!-- Nav menu  -->
+
+    <div class="flex flex-col justify-center space-y-2 items-center bg-gray-100 py-2 mx-10 mt-10 rounded-sm">
+        <div class="border rounded-lg w-3/4 h-96 flex flex-col space-y-2 px-2">
+            <h1 class="text-xl text-gray-500">Rented Lockers: <span class="text-black">0</span></h1>
+            <a href="/lockers">Rent a Locker Bitch</a>
+        </div>
+        
+    </div>
+
+    
 </div>
