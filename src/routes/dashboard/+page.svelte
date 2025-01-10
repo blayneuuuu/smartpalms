8755e1a8c81f1dc7636ad2013c579303e0a9c22a
gregoryerrl <gregoryerrl@gmail.com>
2025-01-10 23:52:41 +0800

    Simple dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
 	import {UserProfile} from 'svelte-clerk'
 	import {UserButton} from 'svelte-clerk'
-    export let data;
+    import { useClerkContext } from 'svelte-clerk';
 
-    console.log(data.user);
+    let id = $state("");
+    let email = $state("");
+    let fullName = $state("");
+
+// Do not destructure context or you'll lose reactivity!
+    const ctx = useClerkContext();
+    const data = $derived(ctx);
     
-    
+    if (data.user){
+    id = data.user.id;
+    email = data?.user.emailAddresses[0].emailAddress;
+    fullName = data?.user.fullName || "User";
+    }
 </script>
 
 <div>
+    <span>{fullName}</span>
+    <span>{email}</span>
+    <span>{id}</span>
     <UserButton afterSignOutUrl="/"/>
-	<UserProfile/>
 </div>
