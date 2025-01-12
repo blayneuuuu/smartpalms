94b545d703ef1db60aa8eda0c85d623ba84d2c34
gregoryerrl <gregoryerrl@gmail.com>
2025-01-12 15:22:05 +0800

    Try: fix naming

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     paths: {
-      base: process.env.NODE_ENV === "production" ? "/smartpalms-svelte" : "",
+      base: process.env.NODE_ENV === "production" ? "/smartpalms" : "",
     },
     adapter: adapter(),
   },
